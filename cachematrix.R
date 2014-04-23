d804827a127059fd00efb133ad2e25abe072491e
bschousek <brian@schousek.com>
2014-04-22 22:00:38 -0500

    Code works, not yet documented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix accepts a matrix and returns a 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(stored.matrix = matrix()) {
+    solution<-NULL
+    set <- function(input.matrix) {
+        stored.matrix<<-input.matrix
+        solution <<- NULL
+    }
+    get <- function() stored.matrix
+    setsolution <- function(solve) solution <<- solve
+    getsolution <- function() solution
+    list (set=set, get=get, setsolution=setsolution, getsolution=getsolution)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(stored.matrix, ...) {
+        solution<- stored.matrix$getsolution()
+        if(!is.null(solution)) {
+            message("getting cached data")
+            return(solution)
+        }
+        data <- stored.matrix$get()
+        solution <- solve(data,...)
+        stored.matrix$setsolution(solution)
+        solution
         ## Return a matrix that is the inverse of 'x'
 }
